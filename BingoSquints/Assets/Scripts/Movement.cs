f11c9ca0327b17503d2b96b81593efc13684903f
Benjamin Gillott <bg357@cornell.edu>
2021-03-26 01:04:20 -0400

    comments, some scene change

--- BingoSquints/Assets/Scripts/Movement.cs
+++ BingoSquints/Assets/Scripts/Movement.cs
@@ -22,13 +22,16 @@ public class Movement : MonoBehaviour
 
     [Space]
     [Header("Booleans")]
-    public bool canMove;
-    public bool wallGrab;
-    public bool wallJumped;
-    public bool wallSlide;
+    public bool isWallSliding;
     public bool isDashing;
-    private bool groundTouch;
-    public int side = 1;
+    private bool isGrounded;
+
+    public bool wallJumped; //TODO:
+    public int side = 1; //TODO:
+    // public bool canMove; //Artifact
+
+    // public bool wallGrab; 
+    //TODO: Refactor this to match our code, we dont want wall grab
 
 
     void Start(){
@@ -45,31 +48,59 @@ public class Movement : MonoBehaviour
         Vector2 dir = new Vector2(x,y);
         Run(dir);
 
-
-        if(coll.onWall && !coll.onGround) //Wallsliding
+        //Touching a wall and not the ground ==>> Slide down
+        if(coll.onWall && !coll.onGround) 
         {
             WallSlide();
+        }//When no longer wall sliding:
+        else if (isWallSliding && (!coll.onWall || coll.onGround)){
+            //Stop wall sliding
+            // EndWallSlide();
+            isWallSliding = false;
         }
 
+
+        //When the player first touches the ground
+        if (coll.onGround && !isGrounded)
+        {
+            TouchGround();
+        }
+        //When the player first leaves the ground
+        else if(!coll.onGround && isGrounded){
+            LeaveGround();
+        }
+
+        //Jump based on horizontal/vertical inputs
         if(Input.GetButtonDown("Jump"))
         {
-            Jump(dir);
+            if(coll.onGround){
+                Jump(dir);
+            }
+            else if (coll.onWall && !coll.onGround){
+                WallJump(dir);
+            }
         }
 
 
-
-        //Better Jumping : 
+        //Better Jumping Physics: 
         //If the player is falling, fall faster according to fallMultiplier
-        //If the player is jumping, they dont jump as far unless they hold the jump button (acording to lowJumpMultiplier)
         if(rb.velocity.y < 0)
         {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
+        //If the player is jumping, they dont jump as far unless they hold the jump button (acording to lowJumpMultiplier)
         else if(rb.velocity.y > 0 && !Input.GetButton("Jump"))
         {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
+
+
+
+
+
+
+    /** The following functions are changes in the player's state**/
 
     private void Run(Vector2 dir){
         rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
@@ -81,8 +112,25 @@ public class Movement : MonoBehaviour
         rb.velocity += Vector2.up * jumpForce; 
     }
 
+    private void WallJump(Vector2 dir)
+    {
+        //TODO: Implement wall jumping
+    }
+
+
+    //The player slides down the wall this frame
     private void WallSlide()
     {
+        isWallSliding = true;
         rb.velocity = new Vector2(rb.velocity.x, -slideSpeed);
+    }
+
+
+    //Player touches or leaves the ground - triggers once
+    private void TouchGround(){
+        isGrounded = true;
+    }
+    private void LeaveGround(){
+        isGrounded = false;
     }
 }
